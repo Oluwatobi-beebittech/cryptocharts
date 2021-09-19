87a6ff08d1e18a139f6cf24c8d0a83a2bfc16543
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2021-09-19 21:23:58 +0100

    crypto charts begin

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="crypto">
+      <header className="w-screen h-16 bg-blue-800">
+        <div className="w-3/4 mx-auto h-full">
+          <h1 className="text-white pt-4 font-bold">CryptoCharts</h1>
+        </div>
       </header>
     </div>
   );
