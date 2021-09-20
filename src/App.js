a318556c1963f85c8811aeee8e555e9cd5ee72ab
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2021-09-20 08:12:36 +0100

    crypto charts layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+import Allocation from "./components/allocation/allocation";
+import Analyze from "./components/analyze/analyze";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 function App() {
   return (
     <div className="crypto">
@@ -7,6 +9,17 @@ function App() {
         <div className="w-3/4 mx-auto h-full">
           <h1 className="text-white pt-4 font-bold">CryptoCharts</h1>
         </div>
+
+        <div className="mt-10 rounded-lg bg-blue-50 w-3/4 mx-auto p-3">
+          <BrowserRouter>
+          <Switch>
+            <Route exact path ="/" component= {Allocation} />
+            <Route exact path ="/analyze" component= {Analyze} />
+          </Switch>
+           
+          </BrowserRouter>
+        </div>
+
       </header>
     </div>
   );
